b75b1dc54f9150969b403cbb18233242a8550561
thejunghare <prasadjunghare2@gmail.com>
2023-12-28 00:09:07 +0530

    fin edit profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,5 +16,15 @@ class ProfileController extends Controller {
     public function edit(User $user) {
         return view('profiles.edit', compact('user'));
     }
-    
+
+    public function update(User $user) {
+        $data = request()->validate([
+            'title' => 'required',
+            'bio' => 'required',
+            'url' => 'url',
+        ]);
+        $user->profile->update($data);
+        return redirect("/profile/{$user->id}");
+        // dd($data);
+    }
 }
