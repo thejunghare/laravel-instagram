634cbfd01eccbdbcbd9ca6663a8f611164869064
thejunghare <prasadjunghare2@gmail.com>
2023-12-28 11:33:33 +0530

    fix desgin for create post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,45 +5,28 @@
 <div class="container">
     <form action="/p" enctype="multipart/form-data" method="post">
         @csrf
-        <div class="row">
-            <div class="col-8 offset-2">
-                <div class="row mb-3 text-center">
-                    <h2>Add New Post</h2>
-                </div>
+         <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Caption</span>
+            <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror"
+                   name="caption" value="{{ old('caption') }}"  autocomplete="caption" autofocus>
 
-                <div class="row mb-3">
-                    <label for="caption" class="col-md-4 col-form-label text-md-end">{{ __('Caption') }}</label>
+            @error('caption')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
 
-                    <div class="col-md-6">
-                        <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror"
-                            name="caption" value="{{ old('caption') }}"  autocomplete="caption" autofocus>
+        <div class="input-group">
+            <input id="image" type="file" class="form-control-file @error('image') is-invalid @enderror"
+                   name="image" value="{{ old('image') }}"  autocomplete="image" autofocus>
 
-                        @error('caption')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class=" mb-3 d-flex align-items-end justify-content-evenly">
-                    <div class="">
-                        <!-- <label for="image" class="col-md-4 col-form-label text-md-end">{{ __('Image') }}</label> -->
-                        <input id="image" type="file" class="form-control-file @error('image') is-invalid @enderror"
-                            name="image" value="{{ old('image') }}"  autocomplete="image" autofocus>
-
-                        @error('image')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-
-                    <div class="">
-                        <button type="submit" class="btn btn-primary">Add New Post</button>
-                    </div>
-                </div>
-            </div>
+            @error('image')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+            <button class="btn btn-outline-secondary" type="submit" id="inputGroupFileAddon04">Add Post</button>
         </div>
     </form>
 </div>
