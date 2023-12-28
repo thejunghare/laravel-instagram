227e999e99e57bb2a00688d2b08b90b0cd1f7344
thejunghare <prasadjunghare2@gmail.com>
2023-12-28 15:02:42 +0530

    fix show design

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,15 +3,27 @@
 @section('content')
 
 <div class="container">
-    <div class="card mb-3" >
+    <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-4">
                 <img src="/storage/{{$post->image}}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">{{$post->user->username}}</h5>
-                    <p class="card-text">{{$post->caption}}</p>
+                    <div class="d-flex align-items-center">
+                        <img src="/storage/{{$post->user->profile->profile_picture}}" class="rounded-circle" width="35"
+                             height="20"
+                             alt="">
+                        <a href="/profile/{{$post->user->id}} " class="fw-bold text-decoration-none text-black">{{$post->user->username}}</a>
+                    </div>
+                    <hr>
+                    <p class="card-text">
+                    <span class="fw-bold">
+                    <a href="/profile/{{$post->user->id}}"
+                       class="fw-bold text-decoration-none text-black">{{$post->user->username}}
+                    </a>
+                    </span>
+                        {{$post->caption}}</p>
                     <p class="card-text"><small class="text-body-secondary">{{$post->created_at}}</small></p>
                 </div>
             </div>
