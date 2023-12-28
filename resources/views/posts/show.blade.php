ea39c65833a612014e3824b5f78d5ddde695ae13
thejunghare <prasadjunghare2@gmail.com>
2023-12-28 11:26:06 +0530

    fix show profile view design

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,13 +3,18 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
-        <div class="col-8">
-            <img src="/storage/{{$post->image}}" alt="alt" class="w-100"/>
-        </div>
-        <div class="col-4">
-            <h2>{{$post->user->username}}</h2>
-            <p>{{$post->caption}}</p>
+    <div class="card mb-3" >
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="/storage/{{$post->image}}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{$post->user->username}}</h5>
+                    <p class="card-text">{{$post->caption}}</p>
+                    <p class="card-text"><small class="text-body-secondary">{{$post->created_at}}</small></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
