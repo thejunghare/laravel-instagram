cffe819ef87acfdea56ff0c7d2d61c029fb17263
thejunghare <prasadjunghare2@gmail.com>
2023-12-28 11:17:04 +0530

    fix look for edit  profile

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -8,67 +8,48 @@
         @csrf
         @method('PATCH')
 
-        <div class="row">
-            <div class="col-8 offset-2">
-                <div class="row mb-3 text-center">
-                    <h2>Edit Profile</h2>
-                </div>
+        <!--Name-->
+        <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">@</span>
+            <input id="title" type="text" class="form-control @error('title') is-invalid @enderror"
+                   name="title" value="{{ old('title') ?? $user->profile->title }}"  autocomplete="caption" autofocus>
 
-                <!-- title -> Instagram Name --> 
-                <div class="row mb-3">
-                    <label for="title" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="title" type="text" class="form-control @error('title') is-invalid @enderror"
-                               name="title" value="{{ old('title') ?? $user->profile->title }}"  autocomplete="caption" autofocus>
-
-                        @error('title')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <!-- Instagram bio -->
-                <div class="row mb-3">
-                    <label for="bio" class="col-md-4 col-form-label text-md-end">{{ __('Bio') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="bio" type="text" class="form-control @error('bio') is-invalid @enderror"
-                               name="bio" value="{{ old('bio') ?? $user->profile->bio }}"  autocomplete="bio" autofocus>
-
-                        @error('bio')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <!-- Instagram links -->
-                <div class="row mb-3">
-                    <label for="url" class="col-md-4 col-form-label text-md-end">{{ __('Links') }}</label>
-                    <div class="col-6">
-                        <input id="url" type="text" class="form-control @error('url') is-invalid @enderror"
-                               name="url" value="{{ old('url') ?? $user->profile->url }}"  autocomplete="url" autofocus>
-
-                        @error('url')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-                
-                <!--submit button-->
-                <div class="row mb-3">
-                    <button type="submit" class="btn btn-primary">save Profile</button>
-                </div>
-            </div>
+            @error('title')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
         </div>
 
+        <!--links-->
+        <div class="mb-3">
+            <label for="basic-url" class="form-label">Your vanity URL</label>
+            <div class="input-group">
+                <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+                <input id="url" type="text" class="form-control @error('url') is-invalid @enderror"
+                       name="url" value="{{ old('url') ?? $user->profile->url }}"  autocomplete="url" autofocus>
 
+                @error('url')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+            <div class="form-text" id="basic-addon4">Example help text goes outside the input group.</div>
+        </div>
+
+        <!--bio-->
+        <div class="input-group mb-3">
+            <span class="input-group-text">With textarea</span>
+            <textarea aria-label="With textarea" id="bio" class="form-control @error('bio') is-invalid @enderror"
+                      name="bio" value=""  autocomplete="bio" autofocus>{{ old('bio') ?? $user->profile->bio }}</textarea>
+            @error('bio')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Save Profile</button>
     </form>
 </div>
 @endsection
